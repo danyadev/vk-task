ba787f7bdd00445f2e505411aca1fa4dfd654c61
danyadev <danyadev@mail.ru>
2021-06-17 22:45:37 +0300

    chore: initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+{
+  root: true,
+
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2019
+  },
+
+  env: {
+    browser: true,
+    es2020: true
+  },
+
+  extends: ['eslint-config-danyadev']
+}
