919805735579685b4bf647c9c5d5516f282356ac
danyadev <danyadev@mail.ru>
2021-06-18 16:52:08 +0300

    wip: текстовое поле и скелет окошка с эмодзи

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,55 @@
 <template>
   <div class="chat">
     <div class="messages_filler"></div>
-    Поле для ввода текста будет здесь
+
+    <div class="input_wrap">
+      <div
+        ref="inputRef"
+        class="input"
+        role="textbox"
+        contenteditable
+        placeholder="Ваше сообщение"
+      ></div>
+
+      <Transition name="toggle">
+        <EmojiBox v-if="isEmojiBoxOpened" @addEmoji="onAddEmoji" />
+      </Transition>
+
+      <Icon
+        name="emoji"
+        :class="['emoji_btn', { active: isEmojiBoxOpened }]"
+        @click="isEmojiBoxOpened = !isEmojiBoxOpened"
+      />
+    </div>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue';
+
+import EmojiBox from './components/EmojiBox.vue';
+import Icon from './components/Icon.vue';
+
 export default {
+  components: {
+    EmojiBox,
+    Icon
+  },
+
+  setup() {
+    const isEmojiBoxOpened = ref(false);
+    const inputRef = ref();
+
+    function onAddEmoji() {
+      // TODO
+    }
+
+    return {
+      isEmojiBoxOpened,
+      inputRef,
+      onAddEmoji
+    };
+  }
 };
 </script>
 
@@ -19,6 +62,57 @@ export default {
 
 .messages_filler {
   width: 100%;
-  height: 300px;
+  height: 350px;
+}
+
+.input_wrap {
+  position: relative;
+  display: flex;
+  width: 290px;
+  border: 1px solid var(--border_color);
+  border-radius: 4px;
+  line-height: 18px;
+}
+
+.input {
+  flex-grow: 1;
+  overflow-x: hidden;
+  padding: 9px 0 9px 9px;
+  max-height: 150px;
+}
+
+.input:empty::before {
+  content: attr(placeholder);
+  color: var(--placeholder_color);
+  cursor: text;
+}
+
+.input::-webkit-scrollbar {
+  display: none;
+}
+
+.emoji_btn {
+  box-sizing: content-box;
+  height: 20px;
+  padding: 8px;
+  cursor: pointer;
+  color: var(--button_background);
+  opacity: .7;
+  transition: opacity .3s;
+}
+
+.emoji_btn:hover, .emoji_btn.active {
+  opacity: 1;
+}
+
+.toggle-enter-active,
+.toggle-leave-active {
+  transition: all 0.2s ease-out;
+}
+
+.toggle-enter-from,
+.toggle-leave-to {
+  transform: translateY(10px);
+  opacity: 0;
 }
 </style>
---
+++ src/components/EmojiBox.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="emoji_box">
+    <div class="emoji_container"></div>
+
+    <div class="emoji_bottom_nav"></div>
+  </div>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style>
+.emoji_box {
+  position: absolute;
+  width: 289px;
+  bottom: calc(100% + 10px);
+  box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.1);
+}
+
+.emoji_container {
+  height: 245px;
+  border: 1px solid var(--border_color);
+  border-bottom-width: 0;
+  border-radius: 4px 4px 0 0;
+}
+
+.emoji_bottom_nav {
+  height: 36px;
+  background-color: var(--emoji_box_bottom_background);
+  border: 1px solid var(--border_color);
+  border-top-width: 0;
+  border-radius: 0 0 4px 4px;
+}
+
+/* Стрелка */
+
+.emoji_box::before, .emoji_box::after {
+  right: 13px;
+  top: 100%;
+  border: solid transparent;
+
+  position: absolute;
+  content: '';
+  height: 0;
+  width: 0;
+}
+
+.emoji_box::before {
+  border-width: 5.4px;
+  border-top-color: var(--border_color);
+}
+
+.emoji_box::after {
+  margin-right: 0.4px;
+  border-width: 5px;
+  border-top-color: var(--emoji_box_bottom_background);
+  transform: translate3d(0, -1px, 0);
+}
+</style>
